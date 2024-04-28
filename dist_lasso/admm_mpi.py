672d21887ef4068dcad120f850a923b7686695c5
TotoroTron <brian.cheng.1997@gmail.com>
2024-04-28 02:43:25 -0400

    global consensus admm lasso working! tested wide matrix A shape (2000,4000). Bad news, the MPI overhead seems to outweigh the performance boost of distribution with this size of matrix. may need to test much, much larger matrices

--- dist_lasso/admm_mpi.py
+++ dist_lasso/admm_mpi.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import mpi4py.MPI as mpi
+import time
 
 def z_minimization_step(x, y, rho, lambd):
     tau = lambd / rho
@@ -10,28 +11,50 @@ def z_minimization_step(x, y, rho, lambd):
 
 
 
-def MPI_ADMM_Lasso(A, b, x, z, y, rho, lambd, epsilon, max_iters):
-    Nb = A.shape[0] # A has shape (Nb, Nx)
-    Nx = A.shape[1]
+def MPI_ADMM_Lasso(comm, A, b, x_init, z_init, y_init, rho, lambd, epsilon, max_iters):
+    rank = comm.Get_rank()  # Get the rank of the process
+    size = comm.Get_size()  # Total number of processes
+
     list_primal_res = []
     list_dual_res = []
     list_x = []
     list_z = []
 
+    x = x_init
+    z = z_init
+    y = y_init
+
     iters = np.arange(0, max_iters) # max number of iterations
     # (A.T * A + rho * I)^(-1)
-    cached_inv = np.linalg.inv(np.matmul(A.T, A) + rho*np.identity(Nx)) # O(n^3)
+    inv_start_time = time.time()
+    cached_inv = np.linalg.inv(np.matmul(A.T, A) + rho*np.identity(A.shape[1]))
+    inv_elapsed_time = time.time() - inv_start_time
+    print(f"Time to invert matrix (A.T * A + rho * I): {inv_elapsed_time:.5f} seconds")
 
     for k in iters:
-        # primal variable updates
+        # local variable x update
         x = np.matmul(cached_inv, np.matmul(A.T, b) + rho*z - y) # x-minimization step
         z_prev = z.copy() # Store the previous value of z for convergence checking
-        z = z_minimization_step(x, y, rho, lambd) # z-minimization step
 
-        # dual variable update
+        # gather local x and local y from all processes
+        x_all = comm.gather(x, root=0) # a list of vectors
+        y_all = comm.gather(y, root=0)
+        
+        if rank == 0:
+            # average x and y across all processes
+            x_avg = np.mean(x_all, axis=0)
+            y_avg = np.mean(y_all, axis=0)
+            z = z_minimization_step(x_avg, y_avg, rho, lambd) # z-minimization step
+        else:
+            z = None
+
+        # broadcast z back to all processes
+        z = comm.bcast(z, root=0)
+
+        # local dual variable update
         y = y + rho * (x - z)
 
-        # calculate residuals
+        # calculate local residuals
         primal_res = np.linalg.norm(x - z, ord=2)
         dual_res = rho * np.linalg.norm(z - z_prev, ord=2)
 
@@ -56,7 +79,10 @@ def ADMM_Lasso(A, b, x, z, y, rho, lambd, epsilon, max_iters):
 
     iters = np.arange(0, max_iters) # max number of iterations
     # (A.T * A + rho * I)^(-1)
-    cached_inv = np.linalg.inv(np.matmul(A.T, A) + rho*np.identity(Nx)) # O(n^3)
+    start_time = time.time()
+    cached_inv = np.linalg.inv(np.matmul(A.T, A) + rho*np.identity(Nx)) # ~O(n^2+)
+    elapsed_time = time.time() - start_time
+    print(f"Time to invert matrix: {elapsed_time:.5f} seconds")
 
     for k in iters:
         # primal variable updates
--- dist_lasso/lasso_mpi.py
+++ dist_lasso/lasso_mpi.py
@@ -1,6 +1,7 @@
 import socket
 import psutil
 import platform
+import time
 
 import numpy as np
 import pandas as pd
@@ -15,19 +16,19 @@ def print_cpu_info():
     cpu_num = process.cpu_num()  # Get the CPU number on which the current process is running
     return cpu_num
 
-def plot_residuals(A_shape, list_primal_res, list_dual_res, rho, lambd, max_iters):
-    plt.figure(figsize=(8,8))
-    plt.plot(list_primal_res, label="Primal residual")
-    plt.plot(list_dual_res, label="Dual residual")
-    plt.yscale("log")
-    plt.xlabel("Iterations")
-    plt.ylabel("Residual Error")
-    plt.title(f"Convergence Plot (Shape A: (rows={A_shape[0]}, cols={A_shape[1]})(rho={rho}, lambda={lambd})")
-    plt.legend()
-    plt.grid(which='both', axis='both')  # Enable grid for log scale
-    plt.ylim(10**-15, 10**3)  # Limit the y-axis to this range.
-    plt.xlim(0, max_iters)
-    plt.savefig('residuals.png', format='png', dpi=300, transparent=True, quality=95)
+def plot_residuals(list_primal_res, list_dual_res, rho, lambd, max_iters):
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax.plot(list_primal_res, label="Primal residual")
+    ax.plot(list_dual_res, label="Dual residual")
+    ax.set_yscale("log")
+    ax.set_xlabel("Iterations")
+    ax.set_ylabel("Residual Error")
+    ax.set_title(f"Convergence Plot (rho={rho}, lambda={lambd})")
+    ax.legend()
+    ax.grid(which='both', axis='both')  # Use logarithmic grid
+    ax.set_ylim(10**-15, 10**3)  # Limit the y-axis to (10**-15, 10**3)
+    ax.set_xlim(0, max_iters)
+    return fig
 
 
 def initialize_variables(Nx, Nb):
@@ -58,10 +59,12 @@ def main():
     cpu_num = print_cpu_info()
     print(f"HELLO WORLD from process {rank} of {size} on {hostname}, running on CPU {cpu_num}, Processor: {platform.processor()}")
 
-    # assert size == 8
-    M = 14000
-    N = 10000
+    M = 2000
+    N = 4000
     Mi = M // size
+
+    lambd = 0.1
+    rho = 1.0
 
     if M % size != 0:
         if rank == 0:
@@ -69,10 +72,63 @@ def main():
         exit()
     
     if rank == 0:
+        print(f"Size of matrix A: (rows={M}, cols={N}), Size of submatrix: (rows={Mi}, cols={N})")
+
         A, b, true_coeffs, x_init, z_init, y_init = initialize_variables(N, M)
-        lambd = 0.1
-        rho = 1.0
-        list_primal_res, list_dual_res, list_x, list_z = admm.ADMM_Lasso(x_init, z_init, y_init, rho=rho, lambd=lambd, epsilon=1e-13, max_iters=3600)
+
+        # VALIDATION TEST SINGLE PROCESS
+        start_time = time.time()
+        list_primal_res, list_dual_res, list_x, list_z = admm.ADMM_Lasso(A, b, x_init, z_init, y_init, rho=rho, lambd=lambd, epsilon=1e-13, max_iters=3600)
+        elapsed_time = time.time() - start_time
+        print(f"Validation test elapsed time: {elapsed_time:.5f} seconds.")
+        fig = plot_residuals(list_primal_res, list_dual_res, rho=rho, lambd=lambd, max_iters=3600)
+        fig.savefig("validation_residuals.png", format="png", dpi=300, transparent=False)
+
+    else:
+        A = None
+        b = None
+        true_coeffs = None
+        x_init = None
+        z_init = None
+        y_init = None
+    
+    ### ================= PARALLEL ZONE ================= ###
+
+    local_A = np.empty((Mi, N), dtype='float64')
+
+    # SCATTER THE ARRAY A
+    start_time = time.time()
+    comm.Scatter(A, local_A, root=0)
+    elapsed_time = time.time() - start_time
+    # Each process now has its own part of the array in recvbuf_A
+    print(f"Time to scatter subblock A of shape {local_A.shape} to process {rank}: {elapsed_time:.5f} seconds")
+
+    # SCATTER THE VECTOR b
+    local_b = np.empty(Mi, dtype='float64')
+    comm.Scatter(b, local_b, root=0)
+    print(f"Process ", {rank}, " has received b of shape", local_b.shape)
+
+    # BROADCAST THE VECTOR x_init
+    local_x = comm.bcast(x_init, root=0)
+    print(f"Process ", {rank}, " has received x of shape", local_x.shape)
+
+    # BROADCAST THE VECTOR z_init
+    local_z = comm.bcast(z_init, root=0)
+    print(f"Process ", {rank}, " has received z of shape", local_z.shape)
+
+    # BROADCAST THE VECTOR y_init
+    local_y = comm.bcast(y_init, root=0)
+    print(f"Process ", {rank}, " has received y of shape", local_y.shape)
+
+
+    start_time = time.time()
+    list_primal_res, list_dual_res, list_x, list_z = admm.MPI_ADMM_Lasso(comm, local_A, local_b, local_x, local_z, local_y, rho=rho, lambd=lambd, epsilon=1e-13, max_iters=3600)
+    elapsed_time = time.time() - start_time
+    print(f"MPI test elapsed time: {elapsed_time:.5f} seconds")
+    fig = plot_residuals(list_primal_res, list_dual_res, rho=rho, lambd=lambd, max_iters=3600)
+    fig.savefig(f"mpi_residuals_process_{rank}.png", format="png", dpi=300, transparent=False)
+
+
 
 
 if __name__ == "__main__":
